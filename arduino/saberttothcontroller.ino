06564a8214c3698b232862d0b94f6729129609ed
josh <mentoce@gmail.com>
2016-11-14 22:33:39 -0800

    added arduino files for the motor controller

---
+++ arduino/saberttothcontroller.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+Servo myservo;
+// This only works in the second controller configuration
+void setup () {
+  myservo.attach(14); // Use PWM pin 14 to control Sabertooth.
+}
+
+void loop() {  
+  // 31 means full power in one direction.
+  // A smaller value won't drive the motor.
+  myservo.write(31); 
+  delay(2000);
+  
+//  // 90 means stopping the motor.
+ myservo.write(90); 
+  delay(2000);  
+    
+  // 159 means full power in the other direction.
+  // A larger value won't drive the motor either.
+  myservo.write(159); 
+  delay(2000);
+
+   myservo.write(90); 
+  delay(2000);  
+}
